fe881c30b5327afa055cea87e6b2ffb9030b0bc4
jency <jencymaheshwari2004@gmail.com>
2025-06-11 00:25:31 +0530

    admin dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { Route, Routes } from "react-router-dom";
 
-import Topbar from "./scenes/global/TopBar";
+import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
 
 import Dashboard from "./scenes/dashboard";
